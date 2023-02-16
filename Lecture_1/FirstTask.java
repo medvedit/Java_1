941e09ec17a0e73f30511a6ac87376cf72f00fe2
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-16 16:05:22 +0300

    Первый коммиит

---
+++ Lecture_1/FirstTask.java
@@ -0,0 +1,16 @@
+/*
+    Запуск программы ctrl + shift + R
+    Форматирование кода alt + cmd + L
+*/
+/**
+ *  Документация к коду пишется в таком виде.
+ *  вот....
+ */
+
+package Java_1.Lecture_1;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
+}
